f714951e52818a832981d263fbbb71c1774792de
Max Gfeller <max.gfeller@gmail.com>
2014-12-10 18:26:11 +0100

    Create folder if it does not exist already

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ var browserify = require('browserify')
 var fs = require('fs')
 
 module.exports = function(path, dir, cb) {
-  var total = 0
   if(path.indexOf('node_modules') === -1) path += '/node_modules'
 
+  fs.exists(dir, function(exists) {
+    if(!exists) return fs.mkdir(dir, function(err) {
+      if(err) return cb(err)
+      execute(path, dir, cb)
+    })
+
+    execute(path, dir, cb)
+  })
+}
+
+function execute(path, dir, cb) {
+  var total = 0
   fs.readdir(path, function(err, modules) {
     if(err) return cb(err)
 
@@ -17,26 +28,26 @@ module.exports = function(path, dir, cb) {
 
       total++
       fs.createReadStream(path + '/' + module + '/package.json')
-        .pipe(concat(function(contents) {
-          try {
-            var pack = JSON.parse(contents)
-          } catch(e) {
-            return cb(e)
-          }
+      .pipe(concat(function(contents) {
+        try {
+          var pack = JSON.parse(contents)
+        } catch(e) {
+          return cb(e)
+        }
 
-          var main = pack.main || 'index.js'
-          var name = pack.name
+        var main = pack.main || 'index.js'
+        var name = pack.name
 
-          var b = browserify({ standalone: name })
-          b.add(path + '/' + module + '/' + main)
-          var target = dir + '/' + name + '.js'
-          var ws = fs.createWriteStream(target)
-          b.bundle().pipe(ws)
-            .on('finish', function() {
-              total--
-              if(total === 0) return cb(null)
-            })
+        var b = browserify({ standalone: name })
+        b.add(path + '/' + module + '/' + main)
+        var target = dir + '/' + name + '.js'
+        var ws = fs.createWriteStream(target)
+        b.bundle().pipe(ws)
+        .on('finish', function() {
+          total--
+          if(total === 0) return cb(null)
+          })
         }))
+      })
     })
-  })
 }
